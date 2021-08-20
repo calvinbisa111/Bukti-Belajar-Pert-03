5fa7ef6bbec72e4d9f437d1ce2ca62ecd51fbc34
calvinbisa111 <88528503+calvinbisa111@users.noreply.github.com>
2021-08-20 15:53:37 +0700

    Bukti Belajar Pert 03

---
+++ Assets/Script/Latihan1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Selamat Siang!");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Latihan2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan2 : MonoBehaviour
+{
+    int nilaiA = 5;
+    int nilaiB = 9;
+    // Use this for initialization
+    void Start()
+    {
+        int totall = nilaiA + nilaiB;
+        Debug.Log("Total " + totall);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
